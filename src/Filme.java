6d215a36373f46889d6fbc8078441809251baf01
Gérald Wessely <geraldmelo01@gmail.com>
2024-11-20 19:27:01 -0300

    Criando instancias

--- src/Filme.java
+++ src/Filme.java
@@ -2,8 +2,21 @@ public class Filme {
      String nome;
      int anoDeLancamento;
     boolean incluidoNoPlano;
-    double avaliacao;
+    double somaDasValiacoes;
     int totalDeAvaliacao;
     int duracaoEmMinuto;
-    
+
+    void exibeFichaTecnica(){
+        System.out.println("Nome do filme: " + nome);
+        System.out.println("Ano de lancamento: " + anoDeLancamento);
+    }
+
+    void avalia(double nota) {
+        somaDasValiacoes += nota;
+        totalDeAvaliacao++;
+    }
+
+    double pegaMedia(){
+        return somaDasValiacoes / totalDeAvaliacao;
+    }
 }
